40d75f0562f4837db2c08d97f1f30ea5d02bb9c1
Li Dai <li.lea.dai@gmail.com>
2020-11-17 20:22:33 -0800

    changed order columns and model validations for the columns

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,11 @@
 class Order < ApplicationRecord
+  validates :name, presence: true
   validates :email,
             format: { with: /^(.+)@(.+)$/, message: "Email invalid"  },
             length: { minimum: 4, maximum: 254 }
   validates :address, presence: true
-  validates :city, presence: true
-  validates :state, presence: true
-  validates :zip, presence: true
+  validates :cc_num, presence: true, numericality: true
+  validates :ccv, presence: true, length: { in: 3..4 }
+  validates :cc_expiration, presence: true
+  validates :billing_zip, presence: true, length: { is: 5 }, numericality: true
 end
--- db/migrate/20201117214707_create_orders.rb
+++ db/migrate/20201117214707_create_orders.rb
@@ -1,16 +1,13 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
+      t.string :name
       t.string :email
       t.string :address
-      t.string :city
-      t.string :state
-      t.integer :zip
       t.integer :cc_num
-      t.integer :ccv
       t.string :cc_expiration
+      t.integer :ccv
       t.integer :billing_zip
-
 
       t.timestamps
     end
