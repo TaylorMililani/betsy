7e6327ace3edf58c3c0398f55170daf8b2ea45ad
India Kato <india.kato@gmail.com>
2020-11-18 09:30:07 -0800

    schema changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_11_17_214707) do
-ActiveRecord::Schema.define(version: 2020_11_17_202130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "merchants", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string "email"
@@ -23,10 +28,13 @@ ActiveRecord::Schema.define(version: 2020_11_17_202130) do
     t.string "city"
     t.string "state"
     t.integer "zip"
-    
-    create_table "merchants", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.string "text_field"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
