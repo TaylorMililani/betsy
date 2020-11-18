7f0d39c7aee0ebcea458f291de22cc702a00801b
India Kato <india.kato@gmail.com>
2020-11-18 12:54:10 -0800

    added added github_login route, created omniauth initializer

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :orders
+  
+  get "auth/github", as: "github_login"
 end
