b6aea634f79318a64435f69aa518d613d717f720
India Kato <india.kato@gmail.com>
2020-11-19 09:45:21 -0800

    added username to users schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_023845) do
+ActiveRecord::Schema.define(version: 2020_11_19_174241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2020_11_19_023845) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "uid"
     t.string "provider"
+    t.string "username"
   end
 
 end
