4016be12aa2ee8f1709308ed7a9f49928c17e03c
Li Dai <li.lea.dai@gmail.com>
2020-11-18 19:38:30 -0800

    created join table

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,42 @@
 class CategoriesController < ApplicationController
+
+  def index
+    @category = Category.all
+  end
+
+  def show
+    @category = Category.find_by(id: params[:id])
+    if @category.nil?
+      flash[:error] = "Category does not exist."
+      head :not_found
+      return
+    end
+    @products = @category.products
+  end
+
+  def new
+    @categories = Category.all
+    @category = Category.new
+  end
+
+
+  def create
+    @category = Category.new(category_params)
+    @categories = Category.all
+    if @category.save
+      flash[:success] = "#{@category.name} has been added!"
+      redirect_to current_user_path
+      return
+    else
+      flash.now[:error] = "Something went wrong. #{@category.errors.messages[:name]}"
+      render :new
+      return
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
