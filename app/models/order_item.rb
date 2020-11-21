b2c4e6e65d565d0049f9525bd67f426903b3e7ce
Taylor Tompkins <taylormililani@gmail.com>
2020-11-21 15:16:56 -0800

    cart view

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,12 @@ class OrderItem < ApplicationRecord
   belongs_to :order
 
   validates :quantity, presence: true, numericality: { greater_than: 0 }
+
+  # def cart_total
+  #   total = 0.0
+  #   @order_items.each do |orderitem|
+  #     total += orderitem.price
+  #   end
+  #   return total
+  # end
 end
