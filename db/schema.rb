2ed8e48fd8a7b5d18307a5e62f23928c72c633ee
Li Dai <li.lea.dai@gmail.com>
2020-11-22 14:45:27 -0800

    deleted category column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_22_001957) do
+ActiveRecord::Schema.define(version: 2020_11_22_224011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 2020_11_22_001957) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.string "category"
     t.float "price"
     t.integer "in_stock"
     t.string "photo"
