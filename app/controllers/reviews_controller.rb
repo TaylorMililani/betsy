bd8353c9a65700efaeac7a7a3904f3c8505648fa
Taylor Tompkins <taylormililani@gmail.com>
2020-11-22 13:41:56 -0800

    working on shopping cart

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,22 +2,29 @@ class ReviewsController < ApplicationController
   # def index
   #   @reviews = Review.all #.find_by(id: product_id) ??
   # end
+  #
+  def new
+    @product = Product.find_by(id: params[:product_id])
+    @review = Review.new
+  end
 
   def create
-    @review = Review.new(review_params)
-    user_id = Product.find_by(id: params[:id]).id
+    @product = Product.find_by(id: params[:product_id])
+    @review = Review.new #(:title, :rating, :text_field)
+    user_id = User.find_by(id: @product[:user_id])
     if session[:user_id] == user_id
       flash.now[:error] = "You can't review your own product!"
+
     elsif @review.save
       flash[:success] = "Thanks for your feedback!"
     else
       flash.now[:error] = "Hmm..something went wrong, your review was not saved"
     end
-    redirect_to root_path # ??
+    redirect_to product_path(@product.id) # ??
   end
 
-  private
-  def review_params
-    return params(:review).permit(:title, :rating, :text_field)
-  end
+  # private
+  # def review_params
+  #   return params(:review).permit(:title, :rating, :text_field)
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_035711) do
+ActiveRecord::Schema.define(version: 2020_11_22_001957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 2020_11_20_035711) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
+    t.bigint "product_id"
+    t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -86,4 +88,5 @@ ActiveRecord::Schema.define(version: 2020_11_20_035711) do
   add_foreign_key "order_items", "orders"
   add_foreign_key "order_items", "products"
   add_foreign_key "products", "users"
+  add_foreign_key "reviews", "products"
 end
