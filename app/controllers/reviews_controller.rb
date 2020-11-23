73db834855c5f84318462a7766ca2560f2402745
Taylor Tompkins <taylormililani@gmail.com>
2020-11-23 10:39:15 -0800

    fixed stylesheet link

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,13 +14,17 @@ class ReviewsController < ApplicationController
     user_id = User.find_by(id: @product[:user_id])
     if session[:user_id] == user_id
       flash.now[:error] = "You can't review your own product!"
+      redirect_to root_path
+      return
+    end
 
-    elsif @review.save
+    if @review.save
       flash[:success] = "Thanks for your feedback!"
     else
       flash.now[:error] = "Hmm..something went wrong, your review was not saved"
     end
     redirect_to product_path(@product.id) # ??
+    return
   end
 
   # private
