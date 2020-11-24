ab579cdb5f50a5c12bf2acf2d3433a7619bec37a
Li Dai <li.lea.dai@gmail.com>
2020-11-23 21:12:35 -0800

    oderuser relationship

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,7 +1,7 @@
 class OrderItem < ApplicationRecord
   belongs_to :product
   belongs_to :order
-  # belongs_to :user
+  belongs_to :user
   validates :quantity, presence: true, numericality: { greater_than: 0 }
 
 
