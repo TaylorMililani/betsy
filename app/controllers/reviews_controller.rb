b728135b3393a070abdcebe13f381ae18fe8e14b
Taylor Tompkins <taylormililani@gmail.com>
2020-11-23 19:11:12 -0800

    small change to order_items

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,30 +10,42 @@ class ReviewsController < ApplicationController
   end
 
   def create
+
+    @product = Product.find_by(id: params[:product_id])
+    user_id = product[:user_id]
+
     @review = Review.new(
-        title: params[:title],
-        rating: params[:rating],
-        text_field: params[:text_field],
+        rating: params[:review][:rating],
+        text_field: params[:review][:text_field],
+        title: params[:review][:title],
         product_id: params[:product_id]
     )
 
-    user_id = Product.find_by(id: params[:product_id]).user_id
-
-    if session[:user_id] == user_id
-      flash.now[:error] = "You can't review your own product!"
-    elsif @review.save && (session[:user_id] != merchant_id)
-      flash[:success] = "Your review was added."
-    else
-      flash[:failure] = "Error: Review could not be added."
+    unless session[:user_id] == nil
+      if session[:user_id] == user_id
+        flash.now[:error] = "You can't review your own product!"
+        redirect_back(fallback_location: :back)
+        return
+      end
     end
-    redirect_to
+
+    if @review.save
+      flash[:success] = "Your review was added."
+
+    else
+      flash[:error] = "Hmm, something went wrong"
+      redirect_back(fallback_location: :back)
+      return
+    end
+
+    redirect_to product_path(product.id)
     return
   end
 
   # private
-
+  #
   # def review_params
-  #   return params.require(:review).permit(:title, :rating, :text_field, :product_id)
+  #   params.require(:review).permit(:rating, :text_field, :product_id, :title)
   # end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :product
 
-  validates :rating, presence: true, :inclusion => { :in => [1,2,3,4,5] }
+  # validates :rating, presence: true, :inclusion => { :in => [1,2,3,4,5] }
   # validates :product_id, presence: true
 end
 
